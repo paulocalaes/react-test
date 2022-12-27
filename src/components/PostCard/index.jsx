33bb57c07566cb963d904de96769f01ae49efd10
Paulo Henrique Calaes Oliveira <paulo@radtonics.com>
2022-12-27 16:06:51 +0000

    Initial commit

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+export const PostCard = ({title, cover, body}) =>  {
+
+    return (
+        <div className='post'>
+            <img src={cover} alt={title} />
+            <div className="post-content">
+                <h2>{title}</h2>
+                <p>{body}</p>
+            </div>
+        </div>
+    );
+}
+
+
+
